fa520ba773763e5ae90112112117736543daab1a
黄一骏（21软） <512717388@qq.com>
2023-03-15 16:36:23 +0800

    Initial commit

---
+++ src/main/java/com/ouma/service/BookServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ouma.service;
+
+import com.ouma.mapper.BookMapper;
+import com.ouma.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl {
+    //调用mapper层的操作，设置一个set接口，方便Spring管理
+    private BookMapper bookMapper;
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+    public int addBook(Books book) {
+        return bookMapper.addBook(book);
+    }
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
